b70b05ba9b2b8342ace0f64c2bf2bc6f354c8899
Pranav3782 <surya.nallagonda@gmail.com>
2025-10-22 15:45:15 +0530

    React programs

---
+++ Classwork/React/todo-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Title from "./Title.jsx";
+
+function App() {
+  console.log("Hello app component");
+  
+
+  return (
+    <>
+      <Title/>
+      <Title/>
+      <Title/>
+      <Title/>
+      <Title/>
+    </>
+  )
+}
+
+export default App;
