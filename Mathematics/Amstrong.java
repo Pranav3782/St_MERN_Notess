5ad12095ca307df41f57585102f4ef08834161e8
Pranav3782 <surya.nallagonda@gmail.com>
2025-10-22 15:39:20 +0530

    Mathematics programs

---
+++ Mathematics/Amstrong.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Amstrong {
+    static boolean armstrongNumber(int n) {
+        int am=0;
+        int cude=0;
+        int num=n;
+        while(n!=0){
+            int last=n%10;
+            n=n/10;
+            cude=cude+last*last*last;
+        }
+        am+=cude;
+        return am==num;
+        
+    }
+}
---
+++ Mathematics/Oddeven.java
@@ -0,0 +1,11 @@
+class Oddeven {
+    static boolean isEven(int n) {
+        if(n%2==0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
